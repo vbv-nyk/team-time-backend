1ff4ef612d90338abc502284808b68c627695455
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-05-31 16:08:59 +0530

    Update codegen.ts to scan for files in my src folder

--- codegen.ts
+++ codegen.ts
@@ -1,14 +1,19 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "schema.graphql",
+  schema: "./src/schema.graphql", // Update the schema path to point to src/schema.graphql
   generates: {
     "./resolvers-types.ts": {
       config: {
+        input: {
+          schemaSearchPaths: ["./src/**/*.graphql"], // Update the schemaSearchPaths pattern to include ./src
+          operationSearchPaths: ["./src/**/*.graphql"], // Update the operationSearchPaths pattern to include ./src
+        },
         useIndexSignature: true,
       },
       plugins: ["typescript", "typescript-resolvers"],
     },
   },
 };
+
 export default config;
